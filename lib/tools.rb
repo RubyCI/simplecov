713a4d98771d39a27d92193465638dc247ce96a8
miroslav <miroslav@kolosek.com>
2023-06-06 14:27:07 +0200

    it seems to work, cleaning

--- lib/tools.rb
+++ lib/tools.rb
@@ -1,9 +1,7 @@
 ### simplecov -> start
-STDOUT.print "[SIMPLECOVPATCHER] simplecov -> start\n"
 if ENV['SIMPLECOV_ACTIVE']
   require 'simplecov'
   SimpleCov.at_exit do
-    STDOUT.print "[SIMPLECOVPATCHER] at_exit\n"
     SimpleCov.result.format!
 
     config = {
@@ -12,17 +10,14 @@ if ENV['SIMPLECOV_ACTIVE']
       minimum_coverage_by_file: SimpleCov.minimum_coverage_by_file,
     }
     rspec_runner_index = ENV['TEST_ENV_NUMBER'.freeze].to_i
-    STDOUT.print "[SIMPLECOV TOOLS] at exit\n"
     STDOUT.print "\n|||NEW_MESSAGE|||RUNNING|||SIMPLECOV_CONFIG|||#{JSON.fast_generate([rspec_runner_index, config])}|||\n"
   end
 
   module PrependSc
     def start(*args, &block)
-      STDOUT.print "[SIMPLECOVPATCHER] PrependSc start\n"
       add_filter "tmp"
       merge_timeout 3600
       command_name "RSpec_\#{ENV['TEST_ENV_NUMBER'.freeze].to_i}"
-      STDOUT.print "[SIMPLECOV TOOLS] prependsc start\n"
 
       if ENV['NO_COVERAGE']
         use_merging false
@@ -36,14 +31,11 @@ if ENV['SIMPLECOV_ACTIVE']
 
   module Scf
     def format!
-      STDOUT.print "[SIMPLECOVPATCHER] Scf format\n"
-      STDOUT.print "[SIMPLECOVPATCHER] NO_COVERAGE #{ENV['NO_COVERAGE']}\n"
       return if ENV['NO_COVERAGE']
       rspec_runner_index = ENV['TEST_ENV_NUMBER'.freeze].to_i
 
       original_result_json = JSON.fast_generate(original_result)
       compressed_data = Base64.strict_encode64(Zlib::Deflate.deflate(original_result_json, 9))
-      STDOUT.print "[SIMPLECOV TOOLS] format\n"
       STDOUT.print "\n|||NEW_MESSAGE|||RUNNING|||SIMPLECOV_RESULT|||#{JSON.fast_generate([rspec_runner_index, compressed_data])}|||\n"
       super
     end
